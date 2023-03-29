7cecef2575a734b10d6e60f5fb0465309383b152
Sanghyun Yi <njsharry32@gmail.com>
2023-03-29 23:59:40 +0900

    refactor: MddicationInfo 도메인에 기업명 필드 entpName을 추가한다

--- src/main/java/com/kgu/bravoHealthPark/domain/medicationInfo/domain/MedicationInfo.java
+++ src/main/java/com/kgu/bravoHealthPark/domain/medicationInfo/domain/MedicationInfo.java
@@ -3,13 +3,16 @@ package com.kgu.bravoHealthPark.domain.medicationInfo.domain;
 import com.kgu.bravoHealthPark.domain.state.domain.State;
 import com.kgu.bravoHealthPark.domain.type.domain.Type;
 import com.kgu.bravoHealthPark.domain.user.domain.User;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MedicationInfo {
 
     @Id
@@ -27,7 +30,8 @@ public class MedicationInfo {
     @Enumerated(EnumType.STRING)
     private State state;
 
-    private String name;
+    private String entpName;
+    private String itemName;
     private int tablet; //몇 정
     private int days;
     private LocalDate startDate;
