60343ee954a4895bfd700e5e3c3fc3645fe51261
Sanghyun Yi <njsharry32@gmail.com>
2023-04-10 10:53:44 +0900

    feat: 예외 응답 스펙 정의

---
+++ src/main/java/com/kgu/bravoHealthPark/global/dto/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.kgu.bravoHealthPark.global.dto;
+
+import com.kgu.bravoHealthPark.global.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String errorCode;
+    private String message;
+
+    private ErrorResponse(ErrorCode code) {
+        this.status = code.getStatus().value();
+        this.errorCode = code.getErrorCode();
+        this.message = code.getMessage();
+    }
+
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+}
